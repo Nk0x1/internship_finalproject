7614340575ee6c6dfdd9093bfa95bac0d35f8780
namnair <93312156+namnair@users.noreply.github.com>
2023-10-21 09:53:19 +0530

    added error

--- getpeople.py
+++ getpeople.py
@@ -1,6 +1,7 @@
 # https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/births/
 
 # call api, parse response
+from datetime import datetime
 
 import requests
 from flask import Flask,render_template, request
@@ -12,18 +13,21 @@ def enter():
     return render_template("index.html")
 
 
-
 @app.route('/result', methods=["POST"])
 def result():
-    if request.method=="POST":
+    if request.method == "POST":
         data = request.form['dob']
         if not data:
             return render_template('error.html', error_message="Please enter a valid date.")
-        else:
-            response=requests.get(f"https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/births/{data}")
-            details=response.json()
-            return render_template('final.html',details = details) 
-           
+
+        try:
+            datetime.strptime(data, '%m/%d')
+        except ValueError:
+            return render_template('error.html', error_message="Please enter a valid date in the format MM/DD.")
+
+        response = requests.get(f"https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/births/{data}")
+        details = response.json()
+        return render_template('final.html', details=details)
             
 
 if __name__=="__main__":    
